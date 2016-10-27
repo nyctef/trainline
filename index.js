2db642a9586e17d37ecd3f38b83910f7c6e5a898
Nyctef <nyctef@nyctef.com>
2016-10-27 16:34:47 +0100

    improve rendering
    - requestAnimationFrame needs to be re-queued repeatedly (like
      setTimeout rather than setInterval)
    - only construct the canvas once per reload
    - allow disabling the reload for once this is "done"

--- index.js
+++ index.js
@@ -2,30 +2,39 @@
   
 window.reload = function()
 {
+  var start = performance.now()
   if (window.cleanup_previous) {
     window.cleanup_previous()
   }
 
   //console.log('reloading js')
   loadJs('index.js')
-  var raf_id = window.requestAnimationFrame(function raf_callback() { window.render() })
+  function raf_callback() {
+    window.render()
+    window.raf_callback_id = window.requestAnimationFrame(raf_callback)
+  }
+  window.raf_callback_id = window.requestAnimationFrame(raf_callback)
 
   window.cleanup_previous = function() {
-    window.cancelAnimationFrame(raf_id)
+    window.cancelAnimationFrame(window.raf_callback_id)
   }
+  //console.log('reload took', performance.now() - start, 'ms')
 }
 
 if (!window.has_reload) {
   // this code should only be called once per page load
 
   window.has_reload = true
-  // I think we need an extra layer of indirection here to make sure we pick up new values of window.reload - should check
-  window.setInterval(function reload_callback() { window.reload() }, 250)
+  if (window.setup_reload) {
+    // I think we need an extra layer of indirection here to make sure we pick up new values of window.reload - should check
+    window.setInterval(function reload_callback() { window.reload() }, 250)
+  }
   window.state = {
     clicked_places: [],
     canvas: null,
     dragging: false,
   }
+  window.reload()
 }
 
 function drawGraph(ctx) { 
@@ -37,6 +46,7 @@ function drawGraph(ctx) {
   //ctx.fillRect(50,50,200,50)
 
   window.state.clicked_places.forEach(function(clicked_place) {
+    ctx.fillStyle = 'black'
     ctx.fillRect(clicked_place[0], clicked_place[1], 2, 2)
   })
 }
@@ -53,7 +63,7 @@ function get_actual_coordinates(clickEvent) {
 }
 
 function on_canvas_mousedown(clickEvent) {
-  console.log(clickEvent)
+  //console.log(clickEvent)
   window.state.dragging = true
 }
 
@@ -61,7 +71,7 @@ function on_canvas_mousemove(moveEvent) {
   if (!window.state.dragging) { return }
   //console.log(moveEvent)
   coords = get_actual_coordinates(moveEvent)
-  console.log(`registered mouse at (${coords[0]},${coords[1]})`)
+  //console.log(`registered mouse at (${coords[0]},${coords[1]})`)
   window.state.clicked_places.push(coords)
 }
 
@@ -72,22 +82,30 @@ function on_canvas_mouseup(upEvent) {
 }
 
 window.render = function render() {
+  var start = performance.now()
+  //console.log(Math.round(performance.now()), 'render')
+  var ctx = window.state.canvas.getContext("2d");
+  drawGraph(ctx);
+  //console.log('render took', performance.now() - start, 'ms')
+}
 
+function init() {
   var body = document.getElementsByTagName("body")[0];
   while (body.hasChildNodes()) {
       body.removeChild(body.lastChild);
   }
-
   var canvas = window.state.canvas = document.createElement("canvas");
   canvas.width = 650;
   canvas.height = 530;
-  var ctx = canvas.getContext("2d");
-  drawGraph(ctx);
   body.appendChild(canvas);
 
   canvas.addEventListener('mousedown', on_canvas_mousedown)
   canvas.addEventListener('mousemove', on_canvas_mousemove)
   canvas.addEventListener('mouseup', on_canvas_mouseup)
+
+  render()
 }
+
+init()
 
 })(window)
